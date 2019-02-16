995987eb2bdfd0bb483fe0f5570caad34c74f081
babzq8 <brittneybarrick@gmail.com>
2019-02-16 13:19:49 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NONE_TYPE } from '@angular/compiler/src/output/output_ast';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
